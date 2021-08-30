db1c25a18ec345df66294cacd6cba194e014df27
Animesh Kumar <animesh.kumar@successive.tech>
2021-08-30 21:44:08 +0530

    feat: Day 5 - Packages in Node.js

---
+++ extra/utils/permission.js
@@ -0,0 +1,20 @@
+import {permissions} from "../constant.js";
+
+const hasPermission = (moduleName, role, permissionType) =>{
+
+    let found = false
+    const obj = permissions[moduleName]         // fetching the module from name
+    const permission = obj[permissionType]      // fetching type of permission
+    permission.forEach(element => {     // iterating to check permission
+        if(element == role){
+                found = true        // if role is found in permission the return true
+                return found
+        }
+    });
+
+    return found            // if not found, automatically return false
+}
+
+//console.log(hasPermission('getUsers','trainer','read'))
+export default hasPermission
+
