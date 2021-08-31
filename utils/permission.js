c94b285054288cd0c5b69f2fb5b4c094180d97db
Animesh Kumar <animesh.kumar@successive.tech>
2021-08-31 20:09:26 +0530

    feat: Validation and Permission

--- utils/permission.js
+++ utils/permission.js
@@ -1,4 +1,4 @@
-permissions = 
+const permissions = 
 {
     'getUsers': {
     all: ['head-trainer'],
@@ -8,29 +8,11 @@ permissions =
     }
 }
 
-const hasPermission = (moduleName, role, permissionType) =>{
-    
-    let found = false
-    const obj = permissions[moduleName]         // fetching the module from name
-    const permission = obj[permissionType]      // fetching type of permission
-    permission.forEach(element => {     // iterating to check permission
-        if(element == role){
-                found = true        // if role is found in permission the return true
-                return found
-        }
-    });
 
-    return found            // if not found, automatically return false
+const hasPermission = (moduleName, role, permissionType) =>{
+   const mod = permissions.hasOwnProperty(moduleName);
+   const result = mod && permissions[moduleName][permissionType].includes(role) || permissions[moduleName].all.includes(role)
+   return result
 }
 
-console.log(hasPermission('getUsers','trainer','read'))
-
-/*
-
-    Sample o/p:
-
-    hasPermission('getUsers','trainer','read')
-
-    true
-
- */
+console.log(hasPermission('getUsers','head-trainer','read'))
--- utils/validation.js
+++ utils/validation.js
@@ -3,96 +3,39 @@ const users = [     // Array of Objects, Objects contain email
         traineeEmail: 'trainee1@successive.tech',
         reviewerEmail: 'reviewer1@successive.tech',
     },
-    
+
     {
         traineeEmail: 'trainee2@gmail.com',
         reviewerEmail: 'reviewer3@successive.tech',
     },
-    
+
     {
         traineeEmail: 'trainee3@successive.tech',
         reviewerEmail: 'reviewer3@gmail.com',
     },
     ]
-    
+
     const validateEmail=(email)=>{
-        
+
         var validRegex = /^[a-zA-Z0-9.^]+@successive.tech/;
-    
-        if (validRegex.test(email)) 
-            return true;
-      
-        else return false;
+
+            return validRegex.test(email);
     }
-    
-    var validUsers = []     // List of Valid users
-    var invalidUsers = []   // List of Invalid users
-    
+
+    const validUsers = []     // List of Valid users
+    const invalidUsers = []   // List of Invalid users
+
     const validateUsers=(users)=>{
-    
+
         users.forEach(element => {
-            const {traineeEmail} = element      // using destructing 
-            const {reviewerEmail} = element     // using destructing
-            
-            validateEmail(traineeEmail)==true?validUsers.push(traineeEmail):invalidUsers.push(traineeEmail)
-            validateEmail(reviewerEmail)==true?validUsers.push(reviewerEmail):invalidUsers.push(reviewerEmail)
+            const {traineeEmail,reviewerEmail} = element      
+
+            validateEmail(traineeEmail,reviewerEmail)==true?validUsers.push(traineeEmail,reviewerEmail):invalidUsers.push(traineeEmail),reviewerEmail
             }
         );
     }
-    
-    console.log();
     validateUsers(users)
-    
-    console.log('Valid users:')
-    validUsers.forEach(element => {
-        console.log(element)
-    });
-    console.log('Number of Valid users: \n', validUsers.length)
-    
-    console.log();
-    
-    console.log('Invalid users:')
-    invalidUsers.forEach(element => {
-        console.log(element)
-    });
+    console.log('invalidUsers', invalidUsers)
+    console.log('validUsers', validUsers)    
     console.log('Number of Invalid users: \n', invalidUsers.length)
-    
-    /*
-    
-    For current test case i.e., 
-    
-    [
-    {
-        traineeEmail: 'trainee1@successive.tech',
-        reviewerEmail: 'reviewer1@successive.tech',
-    },
-    
-    {
-        traineeEmail: 'trainee2@gmail.com',
-        reviewerEmail: 'reviewer3@successive.tech',
-    },
-    
-    {
-        traineeEmail: 'trainee3@successive.tech',
-        reviewerEmail: 'reviewer3@gmail.com',
-    },
-    ]
-    
-    
-    O/p:
-    
-    Valid users:
-    trainee1@successive.tech
-    reviewer1@successive.tech
-    reviewer3@successive.tech
-    trainee3@successive.tech
-    Number of Valid users: 
-     4
-    
-    Invalid users:
-    trainee2@gmail.com
-    reviewer3@gmail.com
-    Number of Invalid users: 
-     2
-    
-     */
+    console.log('Number of valid users: \n', validUsers.length) 
