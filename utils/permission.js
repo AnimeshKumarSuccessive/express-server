b7a5a2a339abbff785d17dc8ddce803e4e3cb14a
Animesh Kumar <animesh.kumar@successive.tech>
2021-09-01 13:08:50 +0530

    feat: Export and Import

--- utils/permission.js
+++
@@ -1,5 +0,0 @@
-const hasPermission = (moduleName, role, permissionType) =>{
-   const mod = permissions.hasOwnProperty(moduleName);
-   const result = mod && (permissions[moduleName][permissionType].includes(role) || permissions[moduleName].all.includes(role))
-   return result
-}
