7dbf268f8c957a60edbe5298bd7973be66f8952e
Animesh Kumar <animesh.kumar@successive.tech>
2021-09-01 12:57:08 +0530

    feat: Export and Import

--- extra/utils/validation.js
+++ extra/utils/validation.js
@@ -1,40 +1,4 @@
-import { validateEmail } from "./helpers.js";
-
-
-/*
-    var validUsers = []     // List of Valid users
-    var invalidUsers = []   // List of Invalid users
-
-    const validateUsers=(users)=>{
-
-        users.forEach(element => {
-            const {traineeEmail} = element      // using destructing 
-            const {reviewerEmail} = element     // using destructing
-
-            validateEmail(traineeEmail)==true?validUsers.push(traineeEmail):invalidUsers.push(traineeEmail)
-            validateEmail(reviewerEmail)==true?validUsers.push(reviewerEmail):invalidUsers.push(reviewerEmail)
-            }
-        );
-        console.log();
-        //  validateUsers(users)
-      
-          console.log('Valid users:')
-          validUsers.forEach(element => {
-              console.log(element)
-          });
-          console.log('Number of Valid users: \n', validUsers.length)
-      
-          console.log();
-      
-          console.log('Invalid users:')
-          invalidUsers.forEach(element => {
-              console.log(element)
-          });
-          console.log('Number of Invalid users: \n', invalidUsers.length)
-    }
-*/
-
-    
+import { validateEmail } from "./helpers.js";    
 
     const validateUsers=(users)=>{
         const validUsers = []     // List of Valid users
@@ -43,7 +7,8 @@ import { validateEmail } from "./helpers.js";
         users.forEach(element => {
             const {traineeEmail,reviewerEmail} = element      
 
-            validateEmail(traineeEmail,reviewerEmail) ? validUsers.push(element): invalidUsers.push(element);
+            validateEmail(traineeEmail) ? validUsers.push(traineeEmail): invalidUsers.push(traineeEmail);
+            validateEmail(reviewerEmail) ? validUsers.push(reviewerEmail): invalidUsers.push(reviewerEmail);
             }
         );
         console.log('invalidUsers', invalidUsers)
@@ -52,7 +17,5 @@ import { validateEmail } from "./helpers.js";
         console.log('Number of valid users: \n', validUsers.length) 
     
     }
-//    validateUsers(users)
-   
-
+  
     export default validateUsers;
