d967062f91a2762106f0ff62416b8ff097c33f17
Animesh Kumar <animesh.kumar@successive.tech>
2021-09-07 08:16:32 +0530

    Feat:First Change

--- extraTs/utils/validation.ts
+++ extraTs/utils/validation.ts
@@ -1,40 +1,4 @@
-import { validateEmail } from "./helpers.js";
-
-
-/*
-    var validUsers = []     // List of Valid users
-    var invalidUsers = []   // List of Invalid users
-
-    const validateUsers=(users)=>{
-
-        users.forEach(element => {
-            const {traineeEmail} = element      // using destructing 
-            const {reviewerEmail} = element     // using destructing
-
-            validateEmail(traineeEmail)==true?validUsers.push(traineeEmail):invalidUsers.push(traineeEmail)
-            validateEmail(reviewerEmail)==true?validUsers.push(reviewerEmail):invalidUsers.push(reviewerEmail)
-            }
-        );
-        console.log();
-        //  validateUsers(users)
-      
-          console.log('Valid users:')
-          validUsers.forEach(element => {
-              console.log(element)
-          });
-          console.log('Number of Valid users: \n', validUsers.length)
-      
-          console.log();
-      
-          console.log('Invalid users:')
-          invalidUsers.forEach(element => {
-              console.log(element)
-          });
-          console.log('Number of Invalid users: \n', invalidUsers.length)
-    }
-*/
-
-    
+import { validateEmail } from "./helpers.js";  
 
     const validateUsers=(users)=>{
         const validUsers = []     // List of Valid users
