8c08fd8396a52e0d17eaabd0430d3c84b0eda184
Animesh Kumar <animesh.kumar@successive.tech>
2021-09-15 18:06:00 +0530

    feat: Setting of Routes Skeletons (Rest API)

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,19 +1,19 @@
 import { config } from 'dotenv';
 config();
 
-import * as Joi from '@hapi/joi';
+// // import * as Joi from '@hapi/joi';
 
-const envVar = Joi.object({    
-  PORT: Joi.number().default(9000),
-  NODE_ENV: Joi.string().default('dev'),
-}).unknown().required();
+// // const envVar = Joi.object({    
+// //   PORT: Joi.number().default(9000),
+// //   NODE_ENV: Joi.string().default('dev'),
+// // }).unknown().required();
 
-const { value: envVars } = envVar.validate(process.env);    
+// const { value: envVars } = envVar.validate(process.env);    
 import IConfig from './Iconfig';
 
 const configuration: IConfig = {
-  port: envVars.PORT,
-  env: envVars.NODE_ENV,
+  port: process.env.PORT,
+  env: process.env.NODE_ENV,
 };
 
 export default Object.freeze(configuration);
--- src/libs/routes/index.ts
+++ src/libs/routes/index.ts
@@ -1,4 +1,4 @@
 import  notFoundRoute  from './NotFoundRoute';
 import  errorHandler  from './ErrorHandler';
 
-export default {errorHandler, notFoundRoute};
+export {errorHandler, notFoundRoute};
