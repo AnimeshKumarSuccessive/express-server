024f799bf4aa6af4a12b4e54567c1f0d41379956
Animesh Kumar <animesh.kumar@successive.tech>
2021-09-24 11:03:53 +0530

    Feat: improve code quality

--- src/server.ts
+++ src/server.ts
@@ -30,9 +30,7 @@ export default class Server {
       console.log('middleWare2');
       next();
     }
-    /**
-     * To setup route
-     */
+
     setupRoutes() {
       const { app } = this;
       
@@ -57,14 +55,8 @@ export default class Server {
       app.use(route.errorHandler);
     }
   
-    /**
-     * To setup Body-Parser
-     */
     initBodyParser() {
-      // parse application/x-www-form-urlencoded
       this.app.use(bodyParser.urlencoded({extended: true}));
-  
-      // parse application/json
       this.app.use(bodyParser.json());
     }
   
