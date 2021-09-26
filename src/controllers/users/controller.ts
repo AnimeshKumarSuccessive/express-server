f8a396024f67c941735738f926b1d6f356bb12ba
Animesh Kumar <animesh.kumar@successive.tech>
2021-09-26 17:09:26 +0530

    feat: Models setup with initial Seed

---
+++ src/controllers/users/controller.ts
@@ -0,0 +1,53 @@
+import { Request, Response, Next } from 'express';
+import { users } from '../../libs/constants';
+import * as jwt from 'jsonwebtoken';
+import config from '../../config/configuration'
+import { request } from 'http';
+
+class Users {
+    get(req: Request, res: Response, next: Next) {
+        return res.status(200).send({ message: 'Fetched data Successfully', data: users });
+    }
+
+    post(req: Request, res: Response, next: Next) {
+        console.log(req.body);
+        const { name, designation, location } = req.body;
+        const result = {
+            name,
+            designation,
+            location,
+        };
+
+        if (!name) {
+            return next({ message: 'Bad request', error: 'Name is required' });
+        }
+        return res.status(200).send({ message: 'users added successfully', data: result });
+    }
+
+    put = (req: Request, res: Response, next: Next): any => {
+        const { id: usersId, ...rest  } = req.body;
+        const data = users.find(({ id }) => id === usersId);
+        if (!data) {
+            return next({ message: 'Bad request', error: 'Id is incorrect' });
+        }
+        const result = { ...data, ...rest };
+        return res.status(200).send({ message: 'users removed successfully', data: result });
+    }
+
+    delete = (req: Request, res: Response, next: Next): any => {
+        const { params: { id: usersId = '' } = {} } = req;
+        const data = users.find(({ id }) => id === usersId);
+        if (!data) {
+           return next({ message: 'Bad request', error: 'Id is incorrect' });
+        }
+        return res.status(200).send({ message: 'user removed successfully', data });
+    }
+
+    createToken(req:Request, res:Response, next:Next){
+        const token = jwt.sign(req.body, config.secret, {expiresIn:'10h'});
+        console.log(token);
+        return next({message: 'Token Succesfully Created', data: { token }, status: 'success'});
+    }
+}
+
+export default new users();
--- src/libs/Database.ts
+++ src/libs/Database.ts
@@ -1,22 +1,24 @@
 import * as mangoose from "mongoose";
-
+import seedData from "./seedData";
 export default class Database{
     public static open(mongoURL) {
-        return new Promise((resolve, reject): any => {
+        return new Promise((resolve, reject) => {
             mangoose.connect(mongoURL,(err) =>{
                 if(err){
-                    console.log(" Error ", err);
+                    console.log("error",err);
                     return reject(err);
                 }
-                console.log(" Succesfully connected to Database ", mongoURL);            
-                return resolve(" Success ");
+                console.log("Succesfully connected to Database",mongoURL);
+                seedData();
+                
+                return resolve("Success");
             });
 
         })
     }
     public static disconnect() {
         mangoose.disconnect((err) => {
-          if (!err) console.log(' Disconnected from MongoDB. ');
+          if (!err) console.log('Disconnected from MongoDB.');
         });
       
     }
