b3f9f226d692edb87390ac91a7b85157a3d0381a
Animesh Kumar <animesh.kumar@successive.tech>
2021-09-26 17:57:18 +0530

    feat: Profile Setup

--- src/controllers/users/route.ts
+++ src/controllers/users/route.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
-import usersRoutes  from './controller';
+import UserRoutes from './controller';
 import validationHandler from '../../libs/validationHandler';
 import validation from './validation';
+import authMiddleWare from '../../libs/routes/authMiddleWare';
+import { users } from '../../libs/constant';
+
 
 const router = Router();
 
-router.get('/', validationHandler(validation.get), usersRoutes.get);
-router.post('/', validationHandler(validation.create), usersRoutes.post);
-router.put('/:id', validationHandler(validation.update), usersRoutes.put);
-router.delete('/:id?', validationHandler(validation.delete), usersRoutes.delete);
-router.post('/createToken', usersRoutes.createToken);
-
+router.get('/', authMiddleWare(users, 'read'), validationHandler(validation.get), UserRoutes.get);
+router.post('/', authMiddleWare(users, 'write'), validationHandler(validation.create), UserRoutes.post);
+router.put('/', authMiddleWare(users, 'write'), validationHandler(validation.update), UserRoutes.put);
+router.delete('/:id', authMiddleWare(users, 'delete'), validationHandler(validation.delete), UserRoutes.delete);
+router.post('/createToken', UserRoutes.createToken);
 export default router;
