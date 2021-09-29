185d719ff089ec2ec330f67bf7a9fd3bf7538c9d
Animesh Kumar <animesh.kumar@successive.tech>
2021-09-29 13:34:18 +0530

    authMiddleWare

--- src/controllers/trainee/route.ts
+++ src/controllers/trainee/route.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import TraineeRoutes  from './controller';
 import validationHandler from '../../libs/validationHandler';
 import validation from './validation';
+import authMiddleWare from '../../libs/routes/authMiddleWare';
 
 const router = Router();
 
-router.get('/', validationHandler(validation.get), TraineeRoutes.get);
-router.post('/', validationHandler(validation.create), TraineeRoutes.post);
-router.put('/', validationHandler(validation.update), TraineeRoutes.put);
-router.delete('/:id', validationHandler(validation.delete), TraineeRoutes.delete);
+router.get('/', authMiddleWare(TraineeRoutes, 'read'), validationHandler(validation.get), TraineeRoutes.get);
+router.post('/', authMiddleWare(TraineeRoutes, 'write'), validationHandler(validation.create), TraineeRoutes.post);
+router.put('/', authMiddleWare(TraineeRoutes, 'write'), validationHandler(validation.update), TraineeRoutes.put);
+router.delete('/:id', authMiddleWare(TraineeRoutes, 'delete'), validationHandler(validation.delete), TraineeRoutes.delete);
 router.post('/createToken', TraineeRoutes.createToken);
 
 export default router;
