c7f1717450508bd5af90194f5394bb2b5ac6b030
Animesh Kumar <animesh.kumar@successive.tech>
2021-09-29 13:15:44 +0530

    feat: add createToken

--- src/controllers/trainee/route.ts
+++ src/controllers/trainee/route.ts
@@ -9,5 +9,6 @@ router.get('/', validationHandler(validation.get), TraineeRoutes.get);
 router.post('/', validationHandler(validation.create), TraineeRoutes.post);
 router.put('/', validationHandler(validation.update), TraineeRoutes.put);
 router.delete('/:id', validationHandler(validation.delete), TraineeRoutes.delete);
+router.post('/createToken', TraineeRoutes.createToken);
 
 export default router;
